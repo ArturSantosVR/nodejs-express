2a08547e05c1fbbe937a366421f69fa185f38da2
unknown <artur.santos@vrsoft.com.br>
2024-03-18 22:13:10 -0300

    conexao banco

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,26 @@
 import express from 'express'
-const app = express()
-const port = 3000
+import { Client } from 'pg'
 
-app.get('/', (req, res) => {
-  res.send('Hello wossd!')
-})
+async function main() {
+    
+    
+    const app = express()
+    const port = 3000
+    
+    const client = new Client()
+    await client.connect()
+    
+    const res = await client.query('SELECT $1::text as message', ['DB Conn is ok!'])
+    console.log(res.rows[0].message) // Hello world!
+    await client.end()
+    
+    app.get('/', (req, res) => {
+        res.send('Hello wossd!')
+    })
+    
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })  
+}
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+main();
